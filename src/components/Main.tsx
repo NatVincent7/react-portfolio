6b57a45099ff1e2321839d619214f8cbf793db7e
Vincent <you@example.com>
2026-03-27 14:25:15 +0700

    feat: Add WhatsApp icon and Resume Icon with the hyperlink on main section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import DescriptionIcon from '@mui/icons-material/Description';
 import profilePic from '../assets/images/profile.jpeg';
 import '../assets/styles/Main.scss';
 
 function Main() {
-
   return (
     <div className="container">
       <div className="about-section">
@@ -18,6 +19,8 @@ function Main() {
             <a href="https://github.com/NatVincent7" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/vincent68/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
             <a href="mailto:nathaniel.vincent68@gmail.com"><EmailIcon/></a>
+            <a href="https://wa.me/6289519839355" target="_blank" rel="noreferrer"><WhatsAppIcon/></a>
+            <a href="/react-portfolio/resume.pdf" target="_blank" rel="noreferrer" download><DescriptionIcon/></a>
           </div>
 
           <h1>Nathaniel Vincent</h1>
@@ -27,6 +30,8 @@ function Main() {
             <a href="https://github.com/NatVincent7" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/vincent68/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
             <a href="mailto:nathaniel.vincent68@gmail.com"><EmailIcon/></a>
+            <a href="https://wa.me/6289519839355" target="_blank" rel="noreferrer"><WhatsAppIcon/></a>
+            <a href="/react-portfolio/resume.pdf" target="_blank" rel="noreferrer" download><DescriptionIcon/></a>
           </div>
         </div>
       </div>
