f3352e89aa7315558b63337f122da27df2b9f039
Vincent <you@example.com>
2026-03-27 14:27:42 +0700

    feat: Add whatsapp and resume icons with the hyperlinks on footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
-import '../assets/styles/Footer.scss'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import DescriptionIcon from '@mui/icons-material/Description';
+import '../assets/styles/Footer.scss';
 
 function Footer() {
   return (
@@ -11,6 +13,8 @@ function Footer() {
         <a href="https://github.com/NatVincent7" target="_blank" rel="noreferrer"><GitHubIcon/></a>
         <a href="https://www.linkedin.com/in/vincent68" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
         <a href="mailto:nathaniel.vincent68@gmail.com" target="_blank" rel="noreferrer"><EmailIcon/></a>
+        <a href="https://wa.me/6289519839355" target="_blank" rel="noreferrer"><WhatsAppIcon/></a>
+        <a href="/react-portfolio/resume.pdf" target="_blank" rel="noreferrer" download><DescriptionIcon/></a>
       </div>
       <p>Let's Connect!</p>
     </footer>
